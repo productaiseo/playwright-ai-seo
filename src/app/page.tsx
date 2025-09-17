6444e4f208f77fca5ef5cca06b09fb9ed9f891c3
yusufpirejman@gmail.com <yusufpirejman@gmail.com>
2025-09-17 10:38:47 +0300

    project fundamentals

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+      <main className="flex h-screen w-full bg-black">
+        <Button className="bg-blue-500">Click me</Button>
+      </main>
+  );
+}
